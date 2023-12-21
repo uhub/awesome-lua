a6ae338b3b0a0cd55d98d4dbdbe639673452c3cc
_AMD_ <amd@default.im>
2023-12-21 19:55:25 +0300

    i think its the last attempt

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://amd-nick.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/awesome-lua/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
