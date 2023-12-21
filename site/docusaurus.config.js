3111b8ca6b9edd2d5b7b589df4005c9bc9b25f7f
_AMD_ <amd@default.im>
2023-12-21 19:48:56 +0300

    shit

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://amd-nick.github.io/awesome-lua',
+  url: 'https://amd-nick.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
